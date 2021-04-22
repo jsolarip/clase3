4c18717bdc79a85d0b8779a30f7d6ba4639d9297
jsolarip <jsolariparravicini@solari-parravicini.com.ar>
2021-04-22 15:32:15 -0300

    Termine ejercicio 3

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,37 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+
+type Horas = Number
+type Objetivo = String
+type Presion = Number
+
+presionGolpe :: Horas -> Objetivo -> Presion
+presionGolpe horas objetivo = poder horas / fortaleza objetivo
+
+poder :: Horas -> Number
+poder = (* 15)
+
+fortaleza :: Objetivo -> Number
+fortaleza = (2*) . length
+
+type Golpe = Objetivo -> Number
+
+gomuGomu :: Golpe
+gomuGomu = presionGolpe 180
+
+normalesConsecutivos :: Golpe 
+normalesConsecutivos = presionGolpe 240
+
+objetivoDificil::Objetivo->Bool
+objetivoDificil=(100>).gomuGomu
+
+objetivoAccesible::Objetivo->Bool
+objetivoAccesible=(between 200 400).normalesConsecutivos.focalizarObjetivo
+
+
+focalizarObjetivo::Objetivo->Objetivo
+focalizarObjetivo= take 7
+
+between :: Ord a => a -> a -> a -> Bool
+between bajo alto medio = bajo <= medio && medio <= alto
